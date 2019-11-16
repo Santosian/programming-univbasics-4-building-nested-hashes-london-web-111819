6a1dff38d3643953094f89ec7efc061d64176550
Santiago Molinari <santomolinari@gmail.com>
2019-11-16 00:46:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
 	    :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :key3 => 0,
+        :b_and_o_railroad => {},
         :key4 => 0 
 	    }
 	  }
